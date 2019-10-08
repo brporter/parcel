4de6575d1397b4442caffb8c4b66c192773e8944
Bryan Porter <bryan@bryanporter.com>
2019-10-08 08:10:29 -0500

    fooling around with initial parsing pass

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make
+cp compile_commands.json ../
 make test
 cd ..
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <fstream>
+
+#define BLOCK_TYPE_LENGTH 4
+#define BLOCK_LENGTH_LENGTH 4
+
+
+std::uint32_t read_chunk(std::basic_ifstream<char> &ifs)
+{
+    std::uint32_t value;
+    ifs.read(reinterpret_cast<char*>(&value), 4);
+
+    return value;
+}
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello, world." << std::endl;
+    if (argc != 2)
+    {
+        std::wcerr << L"Usage: " << argv[0] << " [filename]" << std::endl;
+        return 1;
+    }
+
+    std::ifstream file(argv[1], std::ifstream::binary);
+
+    while (file)
+    {
+        uint32_t blockType = read_chunk(file);
+        uint32_t blockLength = read_chunk(file);
+
+        std::wcout << "block type: " << std::hex << blockType << std::endl;
+        std::wcout << "block length: " << blockLength << std::endl;
+
+        file.seekg(blockLength - 8, std::ios_base::cur);
+    }
+
     return 0;
 }
